abf631c4e42cad46d73395bae80f24c106486f64
Vojtech Krasa <vojta.krasa@gmail.com>
2017-06-13 19:51:15 +0200

    cached getRightVersion, colored conflicts

--- src/krasa/mavenrun/analyzer/MyListNode.java
+++ src/krasa/mavenrun/analyzer/MyListNode.java
@@ -1,10 +1,10 @@
 package krasa.mavenrun.analyzer;
 
-import java.util.List;
-import java.util.Map;
-
 import org.jetbrains.idea.maven.model.MavenArtifactNode;
 import org.jetbrains.idea.maven.model.MavenArtifactState;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Vojtech Krasa
@@ -14,28 +14,55 @@ public class MyListNode {
 	protected final String key;
 	protected final List<MavenArtifactNode> value;
 	protected String maxVersion;
+	protected String rightVersion;
+	protected boolean conflict;
 
 	public MyListNode(Map.Entry<String, List<MavenArtifactNode>> s) {
 		key = s.getKey();
 		value = s.getValue();
 		maxVersion = GuiForm.sortByVersion(value);
+		initRightVersion();
+		initConflict();
+	}
+
+	private void initRightVersion() {
+		if (value != null && !value.isEmpty()) {
+			for (MavenArtifactNode mavenArtifactNode : value) {
+				if (mavenArtifactNode.getState() == MavenArtifactState.ADDED) {
+					rightVersion = mavenArtifactNode.getArtifact().getVersion();
+					break;
+				}
+			}
+		}
+	}
+
+	private void initConflict() {
+		String lastVersion = null;
+		if (value != null && !value.isEmpty()) {
+			for (MavenArtifactNode mavenArtifactNode : value) {
+				if (mavenArtifactNode.getState() != MavenArtifactState.EXCLUDED) {
+					String version = mavenArtifactNode.getArtifact().getVersion();
+					if (lastVersion != null && !lastVersion.equals(version)) {
+						conflict = true;
+						break;
+					}
+					lastVersion = version;
+				}
+			}
+		}
+	}
+
+	public boolean isConflict() {
+		return conflict;
 	}
 
 	public String getMaxVersion() {
 		return maxVersion;
 	}
 
-	public String getRightVersion(){
-	    if(value != null && !value.isEmpty()) {
-            for (MavenArtifactNode mavenArtifactNode : value) {
-                if (mavenArtifactNode.getState() == MavenArtifactState.ADDED) {
-                    maxVersion = mavenArtifactNode.getArtifact().getVersion();
-                    break;
-                }
-            }
-        }
-        return maxVersion;
-    }
+	public String getRightVersion() {
+		return rightVersion;
+	}
 
 	@Override
 	public String toString() {
