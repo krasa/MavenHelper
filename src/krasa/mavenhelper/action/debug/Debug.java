9b309cd12ccb621e90c0d9fd084b1dd52869d448
Vojtěch Krása <vojta.krasa@gmail.com>
2018-06-15 00:49:17 +0200

    icon fix 3.7.172.1454.0

--- src/krasa/mavenhelper/action/debug/Debug.java
+++ src/krasa/mavenhelper/action/debug/Debug.java
@@ -12,10 +12,7 @@ public class Debug {
 	public static final List<String> DEBUG_FORK_MODE = Arrays.asList("-DforkMode=never", "-DforkCount=0",
 			"-DreuseForks=false");
 
-	public static final javax.swing.Icon ICON = load("debug.png"); // 16x16
+	public static final javax.swing.Icon ICON = IconLoader.getIcon("src/krasa/mavenhelper/action/debug/debug.png"); // 16x16
 
-	private static javax.swing.Icon load(String path) {
-		return IconLoader.getIcon(path, Debug.class);
-	}
 
 }
