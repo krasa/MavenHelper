526976978c005e31a08f558df7cd40d7ce2d528d
Pei-Tang Huang <tangtheone@gmail.com>
2020-05-13 09:34:53 +0800

    Replace png icons by svg.

--- src/krasa/mavenhelper/icons/MyIcons.java
+++ src/krasa/mavenhelper/icons/MyIcons.java
@@ -3,11 +3,11 @@ package krasa.mavenhelper.icons;
 import com.intellij.openapi.util.IconLoader;
 
 public class MyIcons {
-	public static final javax.swing.Icon ICON = IconLoader.getIcon("/krasa/mavenhelper/icons/debug.png"); // 16x16
-	public static final javax.swing.Icon RUN_MAVEN_ICON = IconLoader.getIcon("/krasa/mavenhelper/icons/runMaven.png"); // 16x16
-	public static final javax.swing.Icon PHASES_CLOSED = IconLoader.getIcon("/krasa/mavenhelper/icons/phasesClosed.png"); // 16x16
-	public static final javax.swing.Icon PLUGIN_GOAL = IconLoader.getIcon("/krasa/mavenhelper/icons/pluginGoal.png"); // 16x16
-	public static final javax.swing.Icon OPEN_TERMINAL = IconLoader.getIcon("/krasa/mavenhelper/icons/OpenTerminal.png"); // 16x16
-	public static final javax.swing.Icon MAVEN_LOGO = IconLoader.getIcon("/krasa/mavenhelper/icons/mavenLogo.png"); // 16x16
+	public static final javax.swing.Icon ICON = IconLoader.getIcon("/krasa/mavenhelper/icons/debug.svg");
+	public static final javax.swing.Icon RUN_MAVEN_ICON = IconLoader.getIcon("/krasa/mavenhelper/icons/runMaven.svg");
+	public static final javax.swing.Icon PHASES_CLOSED = IconLoader.getIcon("/krasa/mavenhelper/icons/phasesClosed.svg");
+	public static final javax.swing.Icon PLUGIN_GOAL = IconLoader.getIcon("/krasa/mavenhelper/icons/pluginGoal.svg");
+	public static final javax.swing.Icon OPEN_TERMINAL = IconLoader.getIcon("/krasa/mavenhelper/icons/OpenTerminal.svg");
+	public static final javax.swing.Icon MAVEN_LOGO = IconLoader.getIcon("/krasa/mavenhelper/icons/mavenLogo.svg");
 
 }
