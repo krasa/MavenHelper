5dcffbf1c46ef69558721da3e5665b1e6d42b13b
StephanPraetsch <stephan.praetsch@googlemail.com>
2020-06-04 22:30:43 +0200

    Lib and resources (#60)
    * move resources to src/main/resources
    
    * move lib to src/main/lib, add src/test/lib with mockito (needs byte-buddy) and assertj
    
    Co-authored-by: Stephan Praetsch <Stephan.Praetsch@mercateo.com>

--- src/test/java/krasa/mavenhelper/analyzer/action/ExcludeDependencyActionTest.java
+++ src/test/java/krasa/mavenhelper/analyzer/action/ExcludeDependencyActionTest.java
@@ -1,14 +1,22 @@
 package krasa.mavenhelper.analyzer.action;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
 public class ExcludeDependencyActionTest {
 
+    class Foo {
+        String get() { return "original"; }
+    }
+
     @Test
     public void dummy() {
-        assertTrue(true);
+        Foo dummy = mock(Foo.class);
+        when(dummy.get()).thenReturn("mocked");
+        assertThat(dummy.get()).isEqualTo("mocked");
     }
 
 }
