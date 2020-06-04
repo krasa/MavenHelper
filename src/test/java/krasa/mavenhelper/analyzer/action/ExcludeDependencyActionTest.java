e2c74c27cd6c0f4f3a3c45820f902a49dc673d3d
StephanPraetsch <stephan.praetsch@googlemail.com>
2020-06-04 20:54:28 +0200

    introduce project structure (#59)
    src/main/java
    src/main/test
    
    Co-authored-by: Stephan Praetsch <Stephan.Praetsch@mercateo.com>

---
+++ src/test/java/krasa/mavenhelper/analyzer/action/ExcludeDependencyActionTest.java
@@ -0,0 +1,14 @@
+package krasa.mavenhelper.analyzer.action;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExcludeDependencyActionTest {
+
+    @Test
+    public void dummy() {
+        assertTrue(true);
+    }
+
+}
